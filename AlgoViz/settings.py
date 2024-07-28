d32df76e4519c506cbf7d961304b4c19c4f9a298
Monowar Hossain Khan <mhkha2022@gmail.com>
2024-07-28 13:56:43 +0600

    Vercel setup build files

--- AlgoViz/settings.py
+++ AlgoViz/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 
 AUTH_USER_MODEL = 'Account.User'
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost']
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
@@ -136,17 +136,16 @@ TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
