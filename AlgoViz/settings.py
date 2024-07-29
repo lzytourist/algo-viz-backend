6f636ee52efa214083acd97cdda542134fc83a60
Monowar Hossain Khan <mhkha2022@gmail.com>
2024-07-29 13:50:05 +0600

    Another fix

--- AlgoViz/settings.py
+++ AlgoViz/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
